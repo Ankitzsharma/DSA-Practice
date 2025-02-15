549cc08d15694273a78d6059a06100377227469d
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-02-15 20:09:10 +0530

    Reverse Without Using Extra Space

--- Array/ReverseArr.java
+++ Array/ReverseArr.java
@@ -1,13 +1,32 @@
 package Array;
-
+// Reverse Array Without Creating Another Arrays.
 public class ReverseArr {
+
     public static void reverse(int arr[]){
-        for(int i=arr.length-1;i>=0;i--){
-            System.out.print(arr[i]+" ");
+        int start=0,end=arr.length-1;
+        while(start<end){
+            
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
         }
+        
     }
     public static void main(String []args){
-        int ar[]={2,4,6,7,9};
-        reverse(ar);
+        int arr[]={2,4,6,7,9,8};
+        System.out.println("Array Before Reverse");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        reverse(arr);  //Function Calling....
+
+        System.out.println("\nArray After Reverse");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
     }
 }
