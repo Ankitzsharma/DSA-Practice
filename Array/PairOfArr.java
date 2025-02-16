8ebb9dfbf34db9437450f7b95944f1f896b9c887
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-02-16 23:37:55 +0530

    Total No. Of Pairs

--- Array/PairOfArr.java
+++ Array/PairOfArr.java
@@ -1,17 +1,20 @@
 package Array;
 
 public class PairOfArr {
-    public static void subarray(int arr[]){
+    public static void printPair(int arr[]){
+        int totalPair=0;
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
                 System.out.print("( "+arr[i]+" , "+arr[j]+" )");
+                totalPair++;
             }
             System.out.println();
         }
+        System.out.println("Total No. Of Pairs: "+totalPair);  // N(N-1)/2
     }
     public static void main(String[] args){
         int ar[]={2,3,4,5,6};
-        subarray(ar);
+        printPair(ar);
 
     }
 }
