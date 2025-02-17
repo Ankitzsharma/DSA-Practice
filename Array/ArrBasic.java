f73b489be692544b2781f0f4dc161259f3cd6d30
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-02-17 16:13:10 +0530

    Basics Of Array

--- Array/ArrBasic.java
+++ Array/ArrBasic.java
@@ -1,7 +1,24 @@
 package Array;
-
+import java.util.Scanner;
 class ArrBasic{
     public static void main(String []args){
+        int arr1[]={2,3,4,5};      // Declaration And Initialization
+        for(int i=0;i<arr1.length;i++){
+            System.out.print(arr1[i]+" ");
+        }
+
         System.out.println("I'm In again");
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];  // Declaration And Initialization
+        // input array...
+        for(int i=0;i<arr.length;i++){
+            arr[i]=s.nextInt();
+        }
+        // output Array...
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
     }
 } 
