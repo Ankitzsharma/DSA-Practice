1f7d929296857bfa5c371b5195f6976acfb2c4f3
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-02-17 16:02:59 +0530

    Max Subarray Sum

---
+++ Array/MaxSubarraySum.java
@@ -0,0 +1,34 @@
+package Array;
+import java.util.*;
+public class MaxSubarraySum {
+    public static void printArr(int arr[]){
+        int min=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){    //Start
+            for(int j=i;j<arr.length;j++){   //End
+                int currSum=0;
+                for(int k=i;k<j;k++){
+                    System.out.print(arr[k]+" ");
+                    currSum+=arr[k];
+                    if(min<currSum){
+                        min=currSum;
+                    }
+                }
+                System.out.println("="+currSum);
+                System.out.println();
+            }
+        }
+        System.out.println("Maximum Sum Is: "+min);
+    }
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int arr[]={1,-2,6,-1,3};
+        // System.out.print("Enter No. Of Element In an Array: ");
+        // int n=s.nextInt();
+        // int ar[]=new int[n];
+        // for (int i = 0; i < ar.length; i++) {
+        //     ar[i]=s.nextInt();
+        // }
+        printArr(arr);
+
+    }
+}
