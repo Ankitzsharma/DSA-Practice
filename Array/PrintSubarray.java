0b0a64b2f95254e25e69e34787202702a37e7bb1
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-04-28 20:57:34 +0530

    update  loop

--- Array/PrintSubarray.java
+++ Array/PrintSubarray.java
@@ -24,6 +24,21 @@ public class PrintSubarray {
         System.out.println("MaxSum: "+maxSum);
         System.out.println("Total Subarray Are: "+totalSubArr); // Sum Of N Numbers(Having Total No. Of Subarray) = N(N+1)/2
     }
+
+
+
+    // public static void subArr(int ar[]){
+    //     for(int i=0;i<ar.length;i++){
+    //         for(int j=i;j<ar.length;j++){
+
+    //             for(int k=i;k<=j;k++){
+    //                 System.out.print(ar[k]);
+    //             }
+    //             System.out.print(", ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
     public static void main(String[] args){
         int ar[]={2,3,4,5,6};
         subArr(ar);
