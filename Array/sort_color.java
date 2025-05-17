76cfa7b392c10d7e81c8d942052839ae2690dfb5
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-05-17 23:53:46 +0530

    Sort_Color

---
+++ Array/sort_color.java
@@ -0,0 +1,12 @@
+package Array;
+
+
+public static void sortColor(int nums){
+    
+}
+public class sort_color {
+    public static void main(String[] args) {
+        int nums[]={2,0,2,1,1,0};
+        sortColor(nums);
+    }
+}
