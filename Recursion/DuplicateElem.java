d089d2de88917e5647ad680379450e0974758e10
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-07-04 15:13:26 +0530

    Duplcate Elements

---
+++ Recursion/DuplicateElem.java
@@ -0,0 +1,31 @@
+package Recursion;
+public class DuplicateElem {
+
+    public static int countDuplicates(int arr[], int n, int i, int count) {
+        // Base case: if we have reached the end of the array
+        if (i >= n) {
+            return count;
+        }
+
+        // Check if the current element is a duplicate
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                break; // No need to check further for this element
+            }
+        }
+
+        // Recursive call to the next element
+        return countDuplicates(arr, n, i + 1, count);
+    }
+
+    public static void main(String[] args){
+        int arr[] = {1, 2, 3, 2, 4, 5, 3, 6, 7, 8, 1};
+        int n = arr.length;
+
+        // Start counting duplicates from the first element
+        int duplicateCount = countDuplicates(arr, n, 0, 0);
+
+        System.out.println("Number of duplicate elements: " + duplicateCount);
+    }
+}
