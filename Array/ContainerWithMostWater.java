feeb5071cd4a352570d30068ee3ab90660c1919f
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-07-15 23:15:26 +0530

    Conatiner With Most Water

---
+++ Array/ContainerWithMostWater.java
@@ -0,0 +1,30 @@
+package Array;
+
+public class ContainerWithMostWater {
+
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, width * currentHeight);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        // Example usage of ContainerWithMostWater
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height)); // Should print 49
+    }
+}
